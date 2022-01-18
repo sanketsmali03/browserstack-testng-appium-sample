bca47bd7923ad8af562dfe6c1cdda001a8f83234
SANKET MALI <sanketsmali@outlook.com>
2022-01-18 21:14:34 +0530

    first commit

---
+++ src/test/java/com/app/test/AppiumLocalTest.java
@@ -0,0 +1,108 @@
+package com.app.test;
+
+import com.browserstack.local.Local;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
+public class AppiumLocalTest {
+
+    private static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    private static final String ACCESS_KEY = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    private static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    private MobileDriver<MobileElement> driver;
+    private Local local;
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupAppAndLocal() throws Exception {
+        PreemptiveBasicAuthScheme authenticationScheme = new PreemptiveBasicAuthScheme();
+        authenticationScheme.setUserName(USERNAME);
+        authenticationScheme.setPassword(ACCESS_KEY);
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://api-cloud.browserstack.com")
+                .setBasePath("app-automate")
+                .setAuth(authenticationScheme)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+        List<String> customIds = get("recent_apps").jsonPath().getList("custom_id");
+        if (customIds == null || !customIds.contains("LocalApp")) {
+            System.out.println("Uploading app...");
+            given()
+                    .header("Content-Type", "multipart/form-data")
+                    .multiPart("file", new File("src/test/resources/app/appdata/LocalSample.apk"), "text/apk")
+                    .param("custom_id", "LocalApp")
+                    .post("upload");
+        } else {
+            System.out.println("Using previously uploaded app...");
+        }
+        System.out.println("Connecting local");
+        local = new Local();
+        Map<String, String> options = new HashMap<>();
+        options.put("key", ACCESS_KEY);
+        local.start(options);
+        System.out.println("Connected. Now testing...");
+    }
+
+    @BeforeTest(alwaysRun = true)
+    public void setupDriver() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("project", "BrowserStack");
+        caps.setCapability("build", "Demo");
+        caps.setCapability("name", "Local Testing - Google Pixel 3");
+        caps.setCapability("device", "Google Pixel 3");
+        caps.setCapability("os_version", "9.0");
+        caps.setCapability("real_mobile", "true");
+        caps.setCapability("app", "LocalApp");
+        caps.setCapability("browserstack.local", "true");
+        driver = new AndroidDriver<>(new URL(URL), caps);
+    }
+
+    @Test
+    public void test() {
+        Wait<MobileDriver<MobileElement>> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NotFoundException.class);
+        MobileElement searchElement = wait.until(d -> d.findElementById("com.example.android.basicnetworking:id/test_action"));
+        searchElement.click();
+        List<MobileElement> allTextViewElements = wait.until(d -> d.findElementsByClassName("android.widget.TextView"));
+        boolean textPresent = allTextViewElements.stream().anyMatch(e -> e.getText().contains("The active connection is wifi."));
+        Assert.assertTrue(textPresent, "Text is not present");
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void closeDriver() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Local testing passed\"}}");
+        driver.quit();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void closeLocal() throws Exception {
+        local.stop();
+        System.out.println("Binary stopped");
+    }
+
+}
---
+++ src/test/java/com/app/test/AppiumParallelTest.java
@@ -0,0 +1,98 @@
+package com.app.test;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
+public class AppiumParallelTest {
+
+    private static final ThreadLocal<MobileDriver<MobileElement>> driverThread = new ThreadLocal<>();
+
+    private static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    private static final String ACCESS_KEY = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    private static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupApp() {
+        PreemptiveBasicAuthScheme authenticationScheme = new PreemptiveBasicAuthScheme();
+        authenticationScheme.setUserName(USERNAME);
+        authenticationScheme.setPassword(ACCESS_KEY);
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://api-cloud.browserstack.com")
+                .setBasePath("app-automate")
+                .setAuth(authenticationScheme)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+        List<String> customIds = get("recent_apps").jsonPath().getList("custom_id");
+        if (customIds == null || !customIds.contains("DemoApp")) {
+            System.out.println("Uploading app...");
+            given()
+                    .header("Content-Type", "multipart/form-data")
+                    .multiPart("file", new File("src/test/resources/app/appdata/WikipediaSample.apk"), "text/apk")
+                    .param("custom_id", "DemoApp")
+                    .post("upload");
+        } else {
+            System.out.println("Using previously uploaded app...");
+        }
+    }
+
+    @BeforeTest(alwaysRun = true)
+    @Parameters({"config", "environment"})
+    public void setupDriver(String configFile, String environment) throws MalformedURLException {
+        JsonPath jsonPath = JsonPath.from(new File("src/test/resources/app/config/" + configFile));
+        Map<String, String> capDetails = new HashMap<>();
+        capDetails.putAll(jsonPath.getMap("capabilities"));
+        capDetails.putAll(jsonPath.getMap("environments." + environment));
+        DesiredCapabilities caps = new DesiredCapabilities(capDetails);
+        caps.setCapability("build", "AppiumParallel"+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+        driverThread.set(new AndroidDriver<>(new URL(URL), caps));
+    }
+
+    @Test
+    public void searchWikipedia() {
+        Wait<MobileDriver<MobileElement>> wait = new FluentWait<>(driverThread.get())
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NotFoundException.class);
+        MobileElement searchElement = wait.until(d -> d.findElementByAccessibilityId("Search Wikipedia"));
+        searchElement.click();
+        MobileElement insertTextElement = wait.until(d -> d.findElementById("org.wikipedia.alpha:id/search_src_text"));
+        insertTextElement.sendKeys("BrowserStack");
+        List<MobileElement> allProductName = wait.until(d -> d.findElementsByClassName("android.widget.TextView"));
+        Assert.assertTrue(allProductName.size() > 0, "Products are not present");
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+        JavascriptExecutor js = (JavascriptExecutor) driverThread.get();
+        js.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Wikipedia search passed\"}}");
+        driverThread.get().quit();
+        driverThread.remove();
+    }
+
+}
